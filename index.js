4dbc4752fe2a8f578c0025633104b4956e3b4453
WoidZero <71274141+woidzero@users.noreply.github.com>
2022-05-19 23:18:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+
+function compile(file) {
+    const fw = fs.readFileSync(path.resolve(__dirname, '..', `${file}.fws`), 'utf-8')
+    return JSON.parse(fw
+        .replaceAll(/\@(.*)\:[ ]+/gi, '')
+        .replaceAll(/[\$+]/g, '"'))
+} 
+
+console.log(compile('test'))
